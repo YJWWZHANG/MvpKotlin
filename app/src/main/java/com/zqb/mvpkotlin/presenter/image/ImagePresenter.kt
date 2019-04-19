1287ad142b35820884eb99e664c2b8e27c16e5b9
zqb <zhangqianbin3@foxmail.com>
2019-04-19 10:44:13 +0800

    1.0.0......Retrofit

--- app/src/main/java/com/zqb/mvpkotlin/presenter/image/ImagePresenter.kt
+++ app/src/main/java/com/zqb/mvpkotlin/presenter/image/ImagePresenter.kt
@@ -1,41 +1,44 @@
 package com.zqb.mvpkotlin.presenter.image
 
-import com.blankj.utilcode.util.LogUtils
 import com.blankj.utilcode.util.Utils
-import com.google.gson.Gson
 import com.zqb.mvpkotlin.R
 import com.zqb.mvpkotlin.base.RxPresenter
 import com.zqb.mvpkotlin.model.bean.ImageBean
-import kotlinx.android.synthetic.main.item_image.view.*
-import okhttp3.*
-import java.io.IOException
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
 import javax.inject.Inject
 
 /**
  *创建时间:2019/4/17 16:22
  */
-class ImagePresenter @Inject constructor(): RxPresenter<ImageContract.View>(), ImageContract.Presenter {
+class ImagePresenter @Inject constructor() : RxPresenter<ImageContract.View>(), ImageContract.Presenter {
 
     private var mCurrentPage = 0
 
     override fun loadImages(position: Int) {
-        OkHttpClient().newCall(
-            Request.Builder().url(
-                "http://pic.sogou.com/pics?reqType=ajax&reqFrom=result&query=" + Utils.getApp().resources.getStringArray(
-                    R.array.tab)[position] + "&start=" + mCurrentPage * 48
-            ).build())
-            .enqueue(object : Callback {
-                override fun onFailure(call: Call, e: IOException) {
-
-                }
-
-                @Throws(IOException::class)
-                override fun onResponse(call: Call, response: Response) {
-                    val imageBean = Gson().fromJson(response.body()!!.string(), ImageBean::class.java)
+        val retrofit2 = Retrofit.Builder()
+            .baseUrl("http://pic.sogou.com")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        val imageApi = retrofit2.create(ImageApi::class.java)
+        val call = imageApi.loadImage(Utils.getApp().resources.getStringArray(R.array.tab)[position], mCurrentPage * 48)
+        Thread {
+            kotlin.run {
+                val imageBean = call.execute().body()
+                if (imageBean != null) {
                     mView?.setImages(imageBean.items)
                     mCurrentPage++
                 }
-            })
+            }
+        }.start()
+    }
+
+    interface ImageApi {
+        @GET("/pics?reqType=ajax&reqFrom=result")
+        fun loadImage(@Query("query") query: String, @Query("start") start: Int): Call<ImageBean>
     }
 
 }
